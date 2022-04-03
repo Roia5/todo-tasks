f4166a993667eab9eac71667b58290c55ef01ede
Roia5 <roeyash5@gmail.com>
2022-04-03 10:56:57 +0000

    get and post done

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
-const quizzesByUserId = {};
+const tasksByUserId = {};
 
-app.get('/quizzes/:id', (req, res) => {
-    res.send(quizzesByUserId[req.params.id] || []);
+app.post('/api/v1/task/', (req, res) => {
+    const taskId = randomBytes(4).toString('hex');
+    const { userId, content } = req.body;
+
+    const tasks = tasksByUserId[userId] || [];
+    tasks.push({ id: taskId, content });
+    tasksByUserId[userId] = tasks;
+    return res.status(201).send(tasks);
 });
 
-app.post('/quizzes/:id', (req, res) => {
-    const quizId = randomBytes(4).toString('hex');
-    const { question } = req.body;
+app.get('/api/v1/tasks', (req, res) => {
+    const { userid } = req.body;
+    res.send(tasksByUserId[userid] || []);
+});
 
-    const quizzes = quizzesByUserId[req.params.id] || [];
-    quizzes.push({ id: quizId, question });
-    quizzesByUserId[req.params.id] = quizzes;
-    return res.status(201).send(quizzes);
+app.get('/api/v1/task/:taskid/', (req, res) => {
+    const { userid } = req.body;
+    const taskid = req.params.taskid;
+    const tasks = tasksByUserId[userid];
+    const task = tasks.find((task) => {
+        return task.id === taskid;
+      });
+    res.send(task);
+});
+app.put('/api/v1/task/:taskid/', (req, res) => {
+    const taskid = req.params.taskid;
+});
+app.patch('/api/v1/task/:taskid/', (req, res) => {
+    const taskid = req.params.taskid;
+});
+app.patch('/api/v1/task/:taskid/', (req, res) => {
+    const taskid = req.params.taskid;
+});
+app.delete('/api/v1/task/:taskid/', (req, res) => {
+    const taskid = req.params.taskid;
 });
 
 app.listen(4001, () => {
