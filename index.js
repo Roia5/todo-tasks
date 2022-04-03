ba5e06823073f54894a17bd370eb4f84598b9dc4
Roia5 <roeyash5@gmail.com>
2022-04-03 14:33:08 +0000

    changed paths and finished get and post

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
 
 const app = express();
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 
 const tasksByUserId = {};
 
-app.post('/api/v1/task/', (req, res) => {
+app.post('/api/v1/:userid/tasks/', (req, res) => {
     const taskId = randomBytes(4).toString('hex');
-    const { userId, content } = req.body;
+    const { content } = req.body;
+    const userId = req.params.userid;
 
     const tasks = tasksByUserId[userId] || [];
     tasks.push({ id: taskId, content });
     tasksByUserId[userId] = tasks;
+    res.setHeader('Content-Type', 'application/json');
     return res.status(201).send(tasks);
 });
 
-app.get('/api/v1/tasks', (req, res) => {
-    const { userid } = req.body;
-    res.send(tasksByUserId[userid] || []);
+app.get('/api/v1/:userid/tasks/', (req, res) => {
+    const userId = req.params.userid;
+    res.send(tasksByUserId[userId] || []);
 });
 
-app.get('/api/v1/task/:taskid/', (req, res) => {
+app.get('/api/v1/:userid/tasks/:taskid/', async (req, res) => {
     const { userid } = req.body;
     const taskid = req.params.taskid;
     const tasks = tasksByUserId[userid];
@@ -33,19 +36,19 @@ app.get('/api/v1/task/:taskid/', (req, res) => {
       });
     res.send(task);
 });
-app.put('/api/v1/task/:taskid/', (req, res) => {
+app.put('/api/v1/:userid/tasks/:taskid/', (req, res) => {
     const taskid = req.params.taskid;
 });
-app.patch('/api/v1/task/:taskid/', (req, res) => {
+app.patch('/api/v1/:userid/tasks/:taskid/', (req, res) => {
     const taskid = req.params.taskid;
 });
-app.patch('/api/v1/task/:taskid/', (req, res) => {
+app.patch('/api/v1/:userid/tasks/:taskid/', (req, res) => {
     const taskid = req.params.taskid;
 });
-app.delete('/api/v1/task/:taskid/', (req, res) => {
+app.delete('/api/v1/:userid/tasks/:taskid/', (req, res) => {
     const taskid = req.params.taskid;
 });
 
-app.listen(4001, () => {
-    console.log('Listening on 4001');
+app.listen(4000, () => {
+    console.log('Listening on 4000');
 });
